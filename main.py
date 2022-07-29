1ee7ba4a6e71773d3f9e4504c352e7ac1a9e0aea
aaran25 <99447618+aaran25@users.noreply.github.com>
2022-07-28 18:05:04 -0700

    updated

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
-Infinite = ("Infinite")
+Infinite = "Infinite"
 pygame.init()
 jumped = False
 acceleration = 6
+altitude = 150
 negative_gravity = 3
-Screen_Dimensions = pygame.display.set_mode((1920,1080))
+Screen_Dimensions = pygame.display.set_mode((1920 , 1080))
 skye = pygame.image.load("sky.webp")
 tileset = pygame.image.load("tileset.png")
 surface_image2 = tileset.get_rect()
@@ -14,49 +15,64 @@ Scaled_Tileset = pygame.transform.scale(tileset, DEFAULT_IMAGE_SIZE)
 spritesprite = pygame.image.load("sprite.png")
 sprite = spritesprite.get_rect()
 print (sprite)
-Scaled_Sprite = pygame.transform.scale(spritesprite, (150, 150))
+Normal_Sprite = pygame.transform.scale(spritesprite, (150, altitude))
+
 
 # TileX, TileY, cropX, cropY, cropWidth, cropHeight
 Cropped_griddle = [1000, 541, 30, 0, 50, 45]
 sum = 1000
 Cropped_Dirtle = [200, 582, 20, 150, 100, 349]
+
+
 def moving(x, y):
-    global acceleration, jumped, Scaled_Sprite
+    global acceleration, jumped, Scaled_Sprite, Normal_Sprite, altitude
     key = pygame.key.get_pressed()
     if key[pygame.K_a]:
         x -= 56
         return x, y
+    if key[pygame.K_d]:
+        x += 59
+        return x, y
+        # Jump
     if key[pygame.K_w]:
-        #y -= 70
+        # y -= 70
 
         if jumped == False:
             jumped = True
         if jumped == True:
-            if y < 470 :
+            # Finished w jump
+            if y == 420 and acceleration < 0:
+                jumped = False
+                acceleration = 6
+                return x, y
+
+            elif y < 421:
                 y = y - acceleration * 6
                 acceleration -= 1
                 print(y)
+                print(jumped)
                 return x, y
             else:
                 return x, y
 
 
-
+    # Crouch
     if key[pygame.K_s]:
-        Scaled_Sprite = pygame.transform.scale(spritesprite, (150, 80))
+        if altitude == 150:
+            altitude = altitude - 70
+            Normal_Sprite = pygame.transform.scale(spritesprite, (150, altitude))
         if y > 420:
             return x, y
         else:
             return x, y+70
-
-
-
-
-    if key[pygame.K_d]:
-        x += 59
-        return x, y
+    #Standing still
     else:
-        return x, y
+        if altitude == 80:
+            altitude = altitude + 70
+            Normal_Sprite = pygame.transform.scale(spritesprite, (150, altitude))
+            return x, y-70
+        else:
+            return x, y
 x = 850
 y = 420
 while True:
@@ -72,16 +88,10 @@ while True:
         sum2 += 72
         Screen_Dimensions.blit(tileset, (0, 0))
     x, y = moving(x, y)
-    Screen_Dimensions.blit(Scaled_Sprite, (x, y))
-
+    Screen_Dimensions.blit(Normal_Sprite, (x, y))
 
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-
-
-
-
-
